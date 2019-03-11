8c6ffb87aeec2c498a6c961fd0e52fe8f3ed77d6
Taebu <mtaebu@gmail.com>
2019-03-11 15:03:52 +0900

    Close #5

---
+++ bossapp/OrderTableViewCell.swift
@@ -0,0 +1,43 @@
+//
+//  OrderTableViewCell.swift
+//  bossapp
+//
+//  Created by ANP on 3/11/19.
+//  Copyright © 2019 ANPR. All rights reserved.
+//
+
+import UIKit
+
+protocol OrderTableViewCellDelegate{
+    func orderCellStateChanged(_ sender: OrderTableViewCell)
+}
+
+class OrderTableViewCell: UITableViewCell {
+    
+    var delegate: OrderTableViewCellDelegate?
+    
+    @IBOutlet weak var storeNameLabel: UILabel!
+    @IBOutlet weak var customerAddressLabel: UILabel!
+    @IBOutlet weak var orderTimeLabel: UILabel!
+    @IBOutlet weak var orderDateLabel: UILabel!
+    @IBOutlet weak var tradeIdLabel: UILabel!
+    @IBOutlet weak var orderButton: UIButton!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    @IBAction func touchUpStateOrderButton(_ sender: UIButton){
+        if let delegate = self.delegate{
+            delegate.orderCellStateChanged(self)
+        }
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
