c8a89577876d4374adc3632017b660f8a8647c37
Taebu <mtaebu@gmail.com>
2019-03-26 17:43:50 +0900

    상세정보 보기 일부 구현 Autolayout 강의 참고

--- bossapp/Controller/OrderViewController.swift
+++ bossapp/Controller/OrderViewController.swift
@@ -8,11 +8,51 @@
 
 import UIKit
 
-class OrderViewController: UIViewController {
+class OrderPayCell: UITableViewCell{
+    
+    @IBOutlet weak var menuName: UILabel!
+    
+    
+    @IBOutlet weak var menuPrice: UILabel!
+    
+    @IBOutlet weak var menuContents: UILabel!
+    
+}
+
+class OrderAddressCell: UITableViewCell{
+    
+    @IBOutlet weak var orderAddressTitle: UILabel!
+    @IBOutlet weak var orderAddressDetail: UILabel!
+    
+}
+class OrderViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    @IBOutlet weak var orderPayCellHeight: NSLayoutConstraint!
+    /*
+    var menuNameData:NSMutableArray = ["세트 4(양념+마늘간장) 1개", "주문금액","배달팁","총 결제금액"]
+    var menuPriceData:NSMutableArray = ["24,000원", "24,000원","2,000원","26,000원"]
+    
+    var orderNameArrayData:NSMutableArray = ["""
+    - 추가선택: 음료사이즈 1.25 추가(1,000원)
+    - 기본:23,000원
+    ""","","",""]
+    */
+    var menuNameData:NSMutableArray = ["사전결제여부", "배달금액","맛집배달팁","총 결제금액"]
+    var menuPriceData:NSMutableArray = ["Y", "2,000원","2,000원","26,000원"]
+    
+    var orderNameArrayData:NSMutableArray = ["","","",""]
+    
+    
+    var menuNameData2:NSMutableArray = ["세트 4(양념+마늘간장) 1개", "주문금액","세트 4(양념+마늘간장) 1개", "주문금액"]
+    var menuPriceData2:NSMutableArray = ["24,000원", "24,000원","24,000원", "24,000원"]
+    
     
     // MARK: - Properties
     var thumbnailImage: UIImage?
     
+    @IBOutlet weak var orderPayTableView: UITableView!
+    
+    @IBOutlet weak var orderAddressTableView: UITableView!
     // MARK: IBOutlets
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var storeName: UILabel!
@@ -96,6 +136,7 @@ extension OrderViewController {
 // MARK: - Life Cycle
 extension OrderViewController {
     
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -111,6 +152,73 @@ extension OrderViewController {
        // self.imageView.image = self.thumbnailImage ?? placeHolderImage
         
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        orderPayTableView.estimatedRowHeight = 100
+        orderPayTableView.rowHeight = UITableView.automaticDimension
+//        orderPayCellHeight.constant = orderPayTableView.contentSize.height
+        
+       // orderAddressTableView.estimatedRowHeight = 100
+       // orderAddressTableView.rowHeight = UITableView.automaticDimension
+        
+    }
+    
+    
+    public func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if tableView == orderPayTableView {
+                return orderNameArrayData.count
+        }else{
+                return menuNameData2.count
+        }
+        
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if tableView == orderPayTableView
+        {
+
+        let cell = orderPayTableView.dequeueReusableCell(withIdentifier: "orderPayCell", for: indexPath) as! OrderPayCell
+        
+        if(orderNameArrayData[indexPath.row % orderNameArrayData.count] as? String == "")
+        {
+            if(cell.menuContents != nil){
+                cell.menuContents.removeFromSuperview()
+                
+            }
+            
+        }else{
+            cell.menuContents.text = orderNameArrayData[indexPath.row % orderNameArrayData.count] as? String
+            
+        }
+        
+        cell.menuName.text =  menuNameData[indexPath.row % menuNameData.count] as? String
+        cell.menuPrice.text =  menuPriceData[indexPath.row % menuPriceData.count] as? String
+        cell.selectionStyle = UITableViewCell.SelectionStyle.none
+        self.view.layoutIfNeeded()
+        
+            return cell
+        }else{
+        
+            let cell = orderAddressTableView.dequeueReusableCell(withIdentifier: "orderAddressCell", for: indexPath) as! OrderAddressCell
+            
+                cell.orderAddressTitle.text = menuNameData2[indexPath.row % menuNameData2.count] as? String
+                
+            
+            
+            cell.orderAddressTitle.text =  menuNameData2[indexPath.row % menuNameData2.count] as? String
+            cell.orderAddressDetail.text =  menuPriceData2[indexPath.row % menuPriceData2.count] as? String
+            cell.selectionStyle = UITableViewCell.SelectionStyle.none
+            self.view.layoutIfNeeded()
+            
+            return cell
+        }
+        
+    }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
@@ -121,5 +229,4 @@ extension OrderViewController {
     }
 }
 ///
-
 
